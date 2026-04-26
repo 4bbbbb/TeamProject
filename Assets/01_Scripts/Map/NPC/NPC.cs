4b17f0d3706de94ee4b883ded759da921171bcb9
4bbbbb <cycr44@gmail.com>
2026-04-27 02:21:25 +0900

    0427. NPC 역할 정리하기

---
+++ Assets/01_Scripts/Map/NPC/NPC.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum NPCType
+{
+    FishShop,
+    GroceryShop,
+    WeaponShop,
+    Fisherman,
+    Pirate,
+    TrainDriver
+
+}
+
+public class NPC : MonoBehaviour
+{
+    [Header("<< NPC 타입 >>")]
+    [SerializeField] private NPCType npcType;
+
+    [Header("<< 상호작용 >>")]    
+    [SerializeField] private float interactionRange = 4f;    
+    [SerializeField] private bool isPlayerInRange;
+
+    private Transform player;
+
+    public NPCType NPCType => npcType;
+    public bool IsPlayerInRange => isPlayerInRange;
+
+    private void Start()
+    {
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+
+        if (playerObj != null)
+            player = playerObj.transform;
+    }
+
+    private void Update()
+    {
+        CheckPlayerDistance();
+
+        if (isPlayerInRange && Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            NPCManager.Instance.InteractWithCurrentNPC();
+        }
+    }
+
+    private void CheckPlayerDistance()
+    {
+        if(player == null) 
+            return;
+
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        bool isInCurrentRange = distance <= interactionRange;
+
+        if (isInCurrentRange == isPlayerInRange)
+            return;
+
+        isPlayerInRange = isInCurrentRange;
+
+
+        isPlayerInRange = isInCurrentRange;
+
+        if (isPlayerInRange)
+        {
+            Debug.Log($"{npcType} NPC 상호작용 범위에 들어옴");
+
+            if (NPCManager.Instance != null)
+                NPCManager.Instance.SetCurrentNPC(this);
+        }
+        else
+        {
+            Debug.Log($"{npcType} NPC 상호작용 범위에서 나감");
+
+            if (NPCManager.Instance != null)
+                NPCManager.Instance.ClearCurrentNPC(this);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!isPlayerInRange)
+            return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, interactionRange);
+    }
+}
---
+++ Assets/01_Scripts/Map/NPC/NPCManager.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class NPCManager : MonoBehaviour
+{
+    public static NPCManager Instance { get; private set; }
+
+    private NPC currentNPC;  // 현재 상호작용 할 수 있는 NPC 
+
+    public NPC CurrentNPC => currentNPC; 
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public void SetCurrentNPC(NPC npc)
+    {
+        currentNPC = npc;
+    }
+
+    public void ClearCurrentNPC(NPC npc)
+    {
+        if(currentNPC == npc)
+        {
+            currentNPC = null;
+        }
+    }
+
+    public void InteractWithCurrentNPC()
+    {
+        if (currentNPC == null)
+            return;
+
+        switch (currentNPC.NPCType)
+        {
+            case NPCType.FishShop:
+                Debug.Log("Fish 상점 NPC와 상호작용");
+                break;
+
+            case NPCType.GroceryShop:
+                Debug.Log("Grocery 상점 NPC와 상호작용");
+                break;
+
+            case NPCType.WeaponShop:
+                Debug.Log("Weapon 상점 NPC와 상호작용");
+                break;
+            case NPCType.Fisherman:
+                Debug.Log("Fisherman NPC와 상호작용");
+                break;
+            case NPCType.Pirate:
+                Debug.Log("Pirate NPC와 상호작용");
+                break;
+            case NPCType.TrainDriver:
+                Debug.Log("TrainDriver NPC와 상호작용");
+                break;
+        }
+    }
+}
