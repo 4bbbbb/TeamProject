b294aed6cd3aaf049565bdde7dc80cb96447d20d
LSH <shyun6240@gmail.com>
2026-04-29 21:12:38 +0900

    PlayerAsset Test

--- Assets/01_Scripts/Tool/Tool_Axe.cs
+++ Assets/01_Scripts/Tool/Tool_Axe.cs
@@ -2,14 +2,30 @@ using UnityEngine;
 
 public class Tool_Axe : MonoBehaviour, ITool
 {
+    [SerializeField] private int toolId;
+
     private ToolData data;
 
     private bool bEquip;
     private bool bUse;
 
+    private void Start()
+    {
+        ToolData toolData = ToolLoadManager.Instance.GetToolData(toolId);
+
+        if (toolData == null)
+        {
+            Debug.LogError($"ToolData is null\nID : {toolId}");
+            return;
+        }
+
+        Init(toolData);
+    }
+
     public void Init(ToolData data)
     {
         this.data = data;
+        Debug.Log($"ToolData 적용 완료\nID : {data.id}\nName : {data.name}");
     }
 
     public void Equip()
